244e27f16f4a004d1829c9b43e05f3c24189f352
Domagoj Gojak <dgojak@gmail.com>
2015-07-09 11:24:40 +0200

    Implemented StateSelectField.

---
+++ Field/StateSelectField.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Formjack\Field;
+
+class StateSelectField extends SelectField {
+
+    /**
+     * @return void
+     */
+    public function init() {
+        parent::init();
+        $this->choices = $this->getStatesArray();
+    }
+
+    /**
+     * @return array
+     */
+    private function getStatesArray() {
+        return array(
+            'AK' => 'Alaska',
+            'AL' => 'Alabama',
+            'AR' => 'Arkansas',
+            'AZ' => 'Arizona',
+            'CA' => 'California',
+            'CO' => 'Colorado',
+            'CT' => 'Connecticut',
+            'DE' => 'Delaware',
+            'FL' => 'Florida',
+            'GA' => 'Georgia',
+            'HI' => 'Hawaii',
+            'IA' => 'Iowa',
+            'ID' => 'Idaho',
+            'IL' => 'Illinois',
+            'IN' => 'Indiana',
+            'KS' => 'Kansas',
+            'KY' => 'Kentucky',
+            'LA' => 'Louisiana',
+            'MA' => 'Massachusetts',
+            'MD' => 'Maryland',
+            'ME' => 'Maine',
+            'MI' => 'Michigan',
+            'MN' => 'Minnesota',
+            'MO' => 'Missouri',
+            'MS' => 'Mississippi',
+            'MT' => 'Montana',
+            'NC' => 'North Carolina',
+            'ND' => 'North Dakota',
+            'NE' => 'Nebraska',
+            'NH' => 'New Hampshire',
+            'NJ' => 'New Jersey',
+            'NM' => 'New Mexico',
+            'NV' => 'Nevada',
+            'NY' => 'New York',
+            'OH' => 'Ohio',
+            'OK' => 'Oklahoma',
+            'OR' => 'Oregon',
+            'PA' => 'Pennsylvania',
+            'RI' => 'Rhode Island',
+            'SC' => 'South Carolina',
+            'SD' => 'South Dakota',
+            'TN' => 'Tennessee',
+            'TX' => 'Texas',
+            'UT' => 'Utah',
+            'VA' => 'Virginia',
+            'VT' => 'Vermont',
+            'WA' => 'Washington',
+            'WI' => 'Wisconsin',
+            'WV' => 'West Virginia',
+            'WY' => 'Wyoming'
+        );
+    }
+
+}
